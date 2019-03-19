68b044156e61d115dafd60670837b20883c50416
Sheng Yunzhou <shengyunzhou@gmail.com>
2019-03-19 08:45:18 +0800

    Add simple table and ignore generated files

--- src/cljs/sg_carpark/views.cljs
+++ src/cljs/sg_carpark/views.cljs
@@ -1,11 +1,26 @@
 (ns sg-carpark.views
   (:require
    [re-frame.core :as re-frame]
-   [sg-carpark.subs :as subs]
-   ))
+   [sg-carpark.subs :as subs]))
+
+
+(def table-data [{:name "John" :age 32} {:name "Mark" :age 41}])
+
+(defn result-table []
+  [:table
+   [:thead
+    [:tr
+     (map (fn [val] [:th val]) (keys (first table-data)))]]
+   [:tbody
+    (map (fn [row]
+           [:tr
+            (map (fn [val] [:td val])
+                 row)])
+         table-data)]])
+
 
 (defn main-panel []
   (let [name (re-frame/subscribe [::subs/name])]
     [:div
      [:h1 "Hello from " @name]
-     ]))
+     [result-table]]))
